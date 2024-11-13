66606e9ac054a4b38128622203d117a80ec15800
Sumin Yeom <109123694+js05157@users.noreply.github.com>
2024-11-13 23:12:45 +0900

    Add files via upload

---
+++ sw-backend/src/test/java/com/sw/sw_backend/SwBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw.sw_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
